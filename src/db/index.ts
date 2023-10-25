92bf5351da8b4b8b705e3627a634a03fa0e10f8d
dmytro.didukh <dmytro.didukh9@gmail.com>
2023-10-25 20:53:05 +0300

    Enhance Post functionality with GraphQL and refactor resolver files
    GraphQL functionality was added to the Post entity and resolver files were updated to reflect the changes. This was done by decorating fields of Post entity with GraphQL's field decorator. As a part of this update, type-graphql imports were utilized as well. Additionally, random.ts was renamed to post.ts, and RandomResolver was refactored to extract posts from the database.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,18 @@
-import { MikroORM } from '@mikro-orm/core';
+import { IDatabaseDriver, MikroORM } from '@mikro-orm/core';
 
 import mikroOrmConfig from '../mikro-orm.config';
 
-async function setupDatabase(): Promise<void> {
+async function setupDatabase(): Promise<MikroORM<IDatabaseDriver>> {
     try {
         const orm = await MikroORM.init(mikroOrmConfig);
         await orm.getMigrator().up();
+
+        console.log('DB CONNECTED');
+
+        return orm;
     } catch (error) {
         console.error('DB CONNECTION ERROR: ', error);
+        throw error;
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,28 @@ import 'reflect-metadata'; // We need it before type-graphql
 import { buildSchema } from 'type-graphql';
 
 import { setupDatabase } from './db';
-import { RandomResolver } from './resolvers';
+import { PostResolver } from './resolvers';
 
 async function start() {
     const app: Express = express();
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [RandomResolver],
+            resolvers: [PostResolver],
             validate: false,
         }),
     });
 
     await apolloServer.start();
-    await setupDatabase();
+    const orm = await setupDatabase();
 
-    app.use('/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(apolloServer));
+    app.use(
+        '/graphql',
+        cors<cors.CorsRequest>(),
+        express.json(),
+        expressMiddleware(apolloServer, {
+            context: async () => ({ em: orm.em }),
+        }),
+    );
 
     app.listen(4000, () => {
         console.log('SERVER STARTED ON PORT 4000');
