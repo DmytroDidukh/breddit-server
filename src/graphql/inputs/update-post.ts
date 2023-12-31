d6ce5ce3049fe8251b1c1e3c2b2b9ba15dbca0bf
dmytro.didukh <dmytro.didukh9@gmail.com>
2023-12-31 13:52:20 +0200

    fix post update validation

--- src/graphql/inputs/update-post.ts
+++ src/graphql/inputs/update-post.ts
@@ -1,14 +1,16 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MinLength, ValidateIf } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
 
 @InputType()
 export class UpdatePostInput {
     @Field(() => String, { nullable: true })
+    @ValidateIf((o) => o.title !== undefined)
     @IsString({ message: 'Title must be a string.' })
     @MinLength(1, { message: 'Title cannot be empty' })
     title?: string;
 
     @Field(() => String, { nullable: true })
+    @ValidateIf((o) => o.title !== undefined)
     @IsString({ message: 'Content must be a string.' })
     @MinLength(1, { message: 'Content cannot be empty' })
     content?: string;
